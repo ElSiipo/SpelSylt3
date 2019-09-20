06468eebd08f74dc43b6abc5c2c07340981d34f3
christian bartelius <christian.b@bokio.se>
2019-09-20 16:57:04 +0200

    Begin fixing MineExplode

--- waterMine.lua
+++ waterMine.lua
@@ -22,16 +22,26 @@ end
 function WaterMine:update(dt)
     -- self.y = self.y - VERTICAL_SPEED * dt
 
-    if self.y == math.floor(self.map.player.y) and self.x == math.floor(self.map.player.x) then
-        self.shouldExplode = true
+    
+
+    if isBetweenValues(self.y, math.floor(self.map.player.y), math.floor(self.map.player.y) + 16) and 
+        isBetweenValues(self.x, math.floor(self.map.player.x), math.floor(self.map.player.x) + 16) then
+            self.shouldExplode = true
     end
+end
+
+function isBetweenValues(currentValue, firstValue, secondValue)
+    return currentValue > firstValue and currentValue < secondValue
 end
 
 function WaterMine:render()
     love.graphics.draw( self.mine_sprite, self.x , self.y)
     love.graphics.setColor(1, 1, 1)
     love.graphics.print("x: " .. self.x .. "y: " .. self.y, self.x, self.y)
-    love.graphics.print("x: " .. math.floor(self.map.player.x) .. "y: " .. math.floor(self.map.player.y), self.x, self.y + 10)
+    -- love.graphics.print("x: " .. math.floor(self.map.player.x) .. "y: " .. math.floor(self.map.player.y), self.x, self.y + 10)
+
+    -- love.graphics.print("x: " .. math.floor(self.x / 16) + 1  .. "y: " .. math.floor(self.y / 16) + 1,  self.x, self.y + 10)
+
 end
 
 function WaterMine:destroy()
